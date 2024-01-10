f17283419d2aeda6f6f775c80af867e9226d0731
damstaylor <damiensabo@live.fr>
2024-01-10 23:09:49 +0100

    Add more info in JobList

--- src/components/JobList.tsx
+++ src/components/JobList.tsx
@@ -1,69 +1,103 @@
 import { useEffect, useState } from 'react'
 
 interface Job {
-  id: number;
-  key: string;
-  reference: string | null;
+  id: number
+  key: string
+  reference: string | null
   board: {
-    key: string;
-    name: string;
-    type: string;
-    subtype: string;
-    environment: string;
-  };
-  board_key: string;
-  name: string;
-  url: string | null;
-  picture: null;
-  summary: string;
+    key: string
+    name: string
+    type: string
+    subtype: string
+    environment: string
+  }
+  board_key: string
+  name: string
+  url: string | null
+  picture: null
+  summary: string
   location: {
-    text: string;
-    lat: number;
-    lng: number;
-    gmaps: null;
-    fields: [];
-  };
-  archive: null;
-  archived_at: null;
-  updated_at: string;
-  created_at: string;
-  sections: [];
-  culture: null;
-  responsibilities: null;
-  requirements: null;
-  benefits: null;
-  interviews: null;
-  skills: { name: string; value: null; type: null }[];
-  languages: { name: string; value: null }[];
-  certifications: { name: string; value: null; type: null }[];
-  courses: [];
-  tasks: { name: string; value: null; type: null }[];
-  interests: [];
-  tags: { name: string; value: string }[];
-  metadatas: [];
-  ranges_float: [];
-  ranges_date: { name: string; value_min: string; value_max: string }[];
+    text: string
+    lat: number
+    lng: number
+    gmaps: null
+    fields: []
+  }
+  archive: null
+  archived_at: null
+  updated_at: string
+  created_at: string
+  sections: []
+  culture: null
+  responsibilities: null
+  requirements: null
+  benefits: null
+  interviews: null
+  skills: {
+    name: string
+    value: null
+    type: null
+  }[]
+  languages: {
+    name: string
+    value: null
+  }[]
+  certifications: {
+    name: string
+    value: null
+    type: null
+  }[]
+  courses: []
+  tasks: {
+    name: string
+    value: null
+    type: null
+  }[]
+  interests: []
+  tags: {
+    name: string
+    value: string
+  }[]
+  metadatas: []
+  ranges_float: []
+  ranges_date: {
+    name: string
+    value_min: string
+    value_max: string
+  }[]
+}
+interface Meta {
+  count: number
+  maxPage: number
+  page: number
+  total: number
 }
 
 const JobList: React.FC = () => {
   const [jobs, setJobs] = useState<Job[]>([])
+  const [meta, setMeta] = useState<Meta | null>(null)
+  const getTagValue = (job: Job, tagName: string) => {
+    const tag = job.tags?.find((tag) => tag.name.toLowerCase() === tagName)
+    return tag?.value ?? ''
+  }
   useEffect(() => {
     const fetchData = async () => {
       try {
         const { REACT_APP_API_KEY, REACT_APP_BASE_URL, REACT_APP_BOARD_KEYS } = process.env
-        const sortOrder = 'desc'
+        const sortOrder: string = 'desc'
         const options = {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
             'X-API-KEY': REACT_APP_API_KEY,
           },
-        }
-        const URL = `${REACT_APP_BASE_URL}/jobs/searching?page=1&limit=30&order_by=${sortOrder}&board_keys=${REACT_APP_BOARD_KEYS}`
-        const response = await fetch(URL, options)
+        } as RequestInit
+        const url: string = `${REACT_APP_BASE_URL}/jobs/searching?page=1&limit=30&order_by=${sortOrder}&board_keys=${REACT_APP_BOARD_KEYS}`
+        const response = await fetch(url, options)
         const data = await response.json()
         if (response.ok) {
           setJobs(data.data.jobs)
+          setMeta(data.meta)
         } else {
           console.error(`Request error: ${data.message}`)
         }
@@ -74,16 +108,26 @@ const JobList: React.FC = () => {
     fetchData()
   }, [])
   return (
-    <div>
+    <div className="job-list">
       <h1>Job list</h1>
       <ul>
         {jobs.map((job) => (
           <li key={job.id}>
             <h2>{job.name}</h2>
-            <p>{job.summary}</p>
+            <h3>{getTagValue(job, 'company')}</h3>
+            <i>{getTagValue(job, 'category')}</i>
+            <h4>{job.location?.text} - {getTagValue(job, 'type')}</h4>
+            <h5>{job.skills.map(tag => tag.name).join(', ')}</h5>
+            <p>{job.summary || 'No job description available :('}</p>
+            <br />
           </li>
         ))}
       </ul>
+      {meta &&
+        <footer>
+          <div>{meta.count} results</div>
+          <div>Page {meta.page}</div>
+        </footer>}
     </div>
   )
 }
